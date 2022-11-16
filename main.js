92c02aae1f32a9a144b778dc157d38db27cec631
skiotto93 <fabio.forenza@hotmail.com>
2022-11-16 15:45:07 +0100

    Fix identazione js

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 const { createApp } = Vue
 
 createApp({
-  data() {
-    return {
-      emails: []
+    data() {
+        return {
+        emails: []
+        }
+    },
+    created() {
+        
+        for(let i = 1; i <= 10; i++) {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((response) => {
+            this.emails.push(response.data.response)
+        })
     }
-  },
-  created() {
-    for(let i = 1; i <= 10; i++) {
-    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-    .then((response) => {
-        this.emails.push(response.data.response)
-    })
-  }
 }
 }).mount('#app')
