c160425fac6b7ed3a195d83c96de265add55ab5e
skiotto93 <fabio.forenza@hotmail.com>
2022-11-17 10:35:32 +0100

    Aggiunto bonus

--- main.js
+++ main.js
@@ -3,16 +3,18 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-        emails: []
+            emails: []
         }
     },
     created() {
-        
         for(let i = 1; i <= 10; i++) {
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then((response) => {
-            this.emails.push(response.data.response)
-        })
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                this.emails.push(response.data.response)
+            })
+        }
+    },
+    mounted() {
+        
     }
-}
 }).mount('#app')
